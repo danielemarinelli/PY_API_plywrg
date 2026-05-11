7e17fac732d7b8340c8b3565e39807fec9ba2e70
Daniele <danielemarinelli1387@gmail.com>
2026-05-11 16:52:07 +0200

    refactor basic & bearer.py tests

---
+++ API_calls/test_08_basic_auth.py
@@ -0,0 +1,42 @@
+import base64
+
+import pytest
+from playwright.sync_api import Playwright
+
+# 1) Basic Authentication 1
+# url: https://httpbin.org/basic-auth/user/pass
+# username: user
+# password : pass
+def test_basic_auth_resp_body_json(playwright: Playwright):
+     request_context = playwright.request.new_context()
+#     process to send user and password in basic Auth
+#     convert from string format to encode format with base64 library and the decode it
+     credentials = base64.b64encode(b"user:pass").decode("utf-8")
+#     user and password will be passed with headers and key ==> 'Authorization'
+     response = request_context.get("https://httpbin.org/basic-auth/user/pass",
+                                    headers={"Authorization": f"Basic {credentials}"}
+                                    )
+     assert response.status == 200
+     response_body = response.json()
+     print("Response body:", response_body)
+
+     request_context.dispose()
+
+
+# 2) Basic Authentication 2
+# url: http://the-internet.herokuapp.com/basic_auth
+# username: admin
+# password : admin
+def test_basic_auth_resp_body_text(playwright: Playwright):
+     request_context = playwright.request.new_context()
+
+     credentials = base64.b64encode(b"admin:admin").decode("utf-8")
+
+     response = request_context.get("http://the-internet.herokuapp.com/basic_auth",
+                                    headers={"Authorization": f"Basic {credentials}"}
+                                    )
+     assert response.status == 200
+     response_body = response.text()   # the response is in TEXT format, not in JSON format, so we use text() method
+     print("Response body:", response_body)
+
+     request_context.dispose()
---
+++ API_calls/test_09_bearer_auth.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import Playwright
+
+# 3) Bearer Token Authentication is used in GitHub , where gets all your details repos
+# url: https://api.github.com/user/repos
+
+def test_bearer_token_auth_github_repos(playwright: Playwright):
+     # to generate the token follow the steps:
+     # a) Settings -> Developer Settings -> Personal access tokens -> Token (Classic) -> Generate new token
+     token="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"
+     # token is passed always in headers and the response will contain all the repos details in JSON form
+     request_context = playwright.request.new_context()
+     response = request_context.get("https://api.github.com/user/repos",
+                                        headers={"Authorization": f"Bearer {token}"}
+                                       )
+     assert response.status==200
+     response_body=response.json()
+
+     print("Response Body(Repositories....)",response_body)
+
+
+# 4) Bearer Token Authentication
+# url: https://api.github.com/user
+
+def test_bearer_token_auth_github_user_info(playwright: Playwright):
+     # returns the details of the user (same token as previous one)
+     token="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"
+
+     request_context = playwright.request.new_context()
+     response = request_context.get("https://api.github.com/user",
+                                        headers={"Authorization": f"Bearer {token}"}
+                                        )
+     assert response.status==200
+     response_body=response.json()
+
+     print("Response Body(User details.....)",response_body)
