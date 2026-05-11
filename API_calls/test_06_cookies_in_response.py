1cadfc93efa273d7b2dd622cab249df9129147bc
Daniele <danielemarinelli1387@gmail.com>
2026-05-11 13:07:34 +0200

    added test for cookies

---
+++ API_calls/test_06_cookies_in_response.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import Playwright
+
+
+def test_cookies_in_response(playwright:Playwright):
+    request_context=playwright.request.new_context()
+
+    response=request_context.get("https://www.google.com/")
+
+    assert response.status_text=="OK"   #assert response.ok
+    assert response.status==200
+
+    # Extract all the cookies from the request context, not from the response (like with the headers)
+    cookies=request_context.storage_state()["cookies"]
+
+    for c in cookies: #usually the name does not change, but value, domain change
+        print(f"{c['name']}==>{c['value']}==>{c['domain']}==>{c['httponly']}")
+
+    # Cookies values are not constant
+    # Check if 'AEC' cookie exists
+    aec_cookie= None
+
+    for c in cookies:   # Verify the existing of cookies and we can't get them from the response, but from the request_context.storage_state()
+        if c["name"] =="AEC":
+            aec_cookie=c # the cookie "AEC" is present in the list
+            break
+
+    # adding assertion with additional description, if assert fails print the message 'Cookie AEC not found'
+    assert aec_cookie is not None, "Cookie 'AEC' not found"
+
+    # Printing details of 'AEC' Cookie
+
+    print(aec_cookie['name'])
+    print(aec_cookie['value'])
+    print(aec_cookie['domain'])
+    print(aec_cookie['path'])
+    print(aec_cookie['expires'])
+
+
+
+
